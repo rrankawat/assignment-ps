da10c450e5c9f1dd63ca56f026b92b35154707d0
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-08-27 18:28:11 +0530

    responsive

--- src/components/launches/LaunchItem.js
+++ src/components/launches/LaunchItem.js
@@ -6,28 +6,40 @@ const LaunchItem = ({ launch }) => {
     <div className="card">
       <img
         src={launch.links.mission_patch ? launch.links.mission_patch : noimg}
-        alt=""
+        alt={launch.mission_name}
       />
 
-      <h4>FalconSat #1</h4>
+      <h4>
+        {launch.mission_name} #{launch.flight_number}
+      </h4>
 
       <h5>Mission Ids:</h5>
       <ul>
-        <li>Item 1</li>
+        {launch.mission_id.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
       </ul>
 
       <div className="card-text">
         <h5>Launch Year:</h5>
-        <span>Something</span>
+        <span>{launch.launch_year}</span>
       </div>
       <div className="card-text">
         <h5>Successful Launch:</h5>
-        <span>Something</span>
+        <span>{launch.launch_success ? "true" : "false"}</span>
       </div>
-      <div className="card-text">
-        <h5>Successful Landing:</h5>
-        <span>Something</span>
-      </div>
+
+      <h5>Successful Landing:</h5>
+      <ul>
+        {launch.rocket.first_stage.cores.map(
+          (item, i) =>
+            item.land_success !== null && (
+              <li key={i}>
+                flight {item.flight} - {item.land_success ? "true" : "false"}
+              </li>
+            )
+        )}
+      </ul>
     </div>
   );
 };
